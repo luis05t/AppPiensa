937cd602f4939e1dadaaf3848583ddd8b44ccf70
David Correa <latinoco@sudamericano.edu.ec>
2025-02-08 01:33:37 -0500

    Alerta en registro

--- app/(auth)/register.tsx
+++ app/(auth)/register.tsx
@@ -5,6 +5,7 @@ import {
   TouchableOpacity,
   ScrollView,
   Platform,
+  Alert,
 } from "react-native";
 import AuthScreen from "@/components/AuthScreen";
 import { useKeyboardVisible } from "@/hooks/useKeyboardVisible";
@@ -15,7 +16,6 @@ import MainTextInput from "@/components/Inputs/MainTextInput";
 import { Link, useRouter } from "expo-router";
 import { http } from "@/services/http";
 import { Token } from "./login";
-import { CustomAlert } from "@/components/Alert";
 
 export default function LoginScreen() {
   const [email, setEmail] = useState("");
@@ -23,9 +23,7 @@ export default function LoginScreen() {
   const [name, setName] = useState("");
   const [showPassword, setShowPassword] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
-  const [showAlert, setShowAlert] = useState(false);
   const { keyboardVisible } = useKeyboardVisible();
-  const router = useRouter();
 
   const handleRegister = async () => {
     setIsLoading(true);
@@ -36,14 +34,14 @@ export default function LoginScreen() {
         password,
       });
       if (accessToken) {
-        router.navigate("/(auth)/login");
-        setShowAlert(true);
+        Alert.alert("Registro Completado", "Ahora puedes iniciar sesión", [
+          { text: "Aceptar" },
+        ]);
       }
     } catch (err) {
       console.log(err);
     } finally {
       setIsLoading(false);
-      setShowAlert(false);
     }
   };
 
@@ -57,14 +55,6 @@ export default function LoginScreen() {
         keyboardShouldPersistTaps="handled"
         scrollEnabled={keyboardVisible}
       >
-        {showAlert && (
-          <CustomAlert
-            visible={showAlert}
-            title="¡Datos Guardados!"
-            message="Ahora puedes iniciar sesión"
-            onClose={() => setShowAlert(false)}
-          />
-        )}
         <View className="flex-1 justify-center p-6">
           {(!keyboardVisible || Platform.OS === "ios") && (
             <View className="items-center mb-12">
